7e21542ce4d3267b71b6c8c5a82315a929998842
A.Eren <84243740+alerntrk@users.noreply.github.com>
2022-08-09 10:51:47 +0300

    Update duzenle.c

--- duzenle.c
+++ duzenle.c
@@ -1,101 +1,207 @@
+
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/mod_devicetable.h>
-#include <linux/property.h>
-#include <linux/platform_device.h>
-#include <linux/of_device.h>
+#include <linux/fs.h>
+#include <linux/version.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/i2c.h>
+#include <linux/kernel.h>
+
+#define DRIVER_NAME "bmp280"
+#define DRIVER_CLASS "bmp280Class"
+
+static struct i2c_adapter * bmp_i2c_adapter = NULL;
+static struct i2c_client * bmp280_i2c_client = NULL;
 
 /* Meta Information */
+MODULE_AUTHOR("Johannes 4Linux");
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Johannes 4 GNU/Linux");
-MODULE_DESCRIPTION("A simple LKM to parse the device tree for a specific device and its properties");
+MODULE_DESCRIPTION("A driver for reading out a BMP280 temperature sensor");
+MODULE_SUPPORTED_DEVICE("NONE");
 
-/* Declate the probe and remove functions */
-static int dt_probe(struct platform_device *pdev);
-static int dt_remove(struct platform_device *pdev);
+/* Defines for device identification */ 
+#define I2C_BUS_AVAILABLE	2		/* The I2C Bus available on the raspberry */
+#define SLAVE_DEVICE_NAME	"ov9282"	/* Device and Driver Name */
+#define BMP280_SLAVE_ADDRESS	0x44		/* BMP280 I2C address */
 
-static struct of_device_id my_driver_ids[] = {
-	{
-		.compatible = "qcom,qrb5165",
-	}, { /* sentinel */ }
+static const struct i2c_device_id bmp_id[] = {
+		{ SLAVE_DEVICE_NAME, 0 }, 
+		{ }
 };
-MODULE_DEVICE_TABLE(of, my_driver_ids);
 
-static struct platform_driver my_driver = {
-	.probe = dt_probe,
-	.remove = dt_remove,
+static struct i2c_driver bmp_driver = {
 	.driver = {
-		.name = "my_device_driver",
-		.of_match_table = my_driver_ids,
-	},
+		.name = SLAVE_DEVICE_NAME,
+		.owner = THIS_MODULE
+	}
 };
 
+static struct i2c_board_info bmp_i2c_board_info = {
+	I2C_BOARD_INFO(SLAVE_DEVICE_NAME, BMP280_SLAVE_ADDRESS)
+};
+
+/* Variables for Device and Deviceclass*/
+static dev_t myDeviceNr;
+static struct class *myClass;
+static struct cdev myDevice;
+
+/* Variables for temperature calculation */
+s32 dig_T1, dig_T2, dig_T3;
+
 /**
- * @brief This function is called on loading the driver 
+ * @brief Read current temperature from BMP280 sensor
+ *
+ * @return temperature in degree
  */
-static int dt_probe(struct platform_device *pdev) {
-	struct device *dev = &pdev->dev;
-	const char *label;
-	int my_value, ret;
+s32 read_temperature(void) {
+	int var1, var2;
+	s32 raw_temp;
+	s32 d1, d2, d3;
 
-	printk("dt_probe - Now I am in the probe function!\n");
+	/* Read Temperature */
+	d1 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFA);
+	d2 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFB);
+	d3 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFC);
+	raw_temp = ((d1<<16) | (d2<<8) | d3) >> 4;
 
-	/* Check for device properties */
-	if(!device_property_present(dev, "label")) {
-		printk("dt_probe - Error! Device property 'label' not found!\n");
-		return -1;
-	}
-	if(!device_property_present(dev, "my_value")) {
-		printk("dt_probe - Error! Device property 'my_value' not found!\n");
-		return -1;
-	}
+	/* Calculate temperature in degree */
+	var1 = ((((raw_temp >> 3) - (dig_T1 << 1))) * (dig_T2)) >> 11;
 
-	/* Read device properties */
-	ret = device_property_read_string(dev, "label", &label);
-	if(ret) {
-		printk("dt_probe - Error! Could not read 'label'\n");
-		return -1;
-	}
-	printk("dt_probe - label: %s\n", label);
-	ret = device_property_read_u32(dev, "my_value", &my_value);
-	if(ret) {
-		printk("dt_probe - Error! Could not read 'my_value'\n");
-		return -1;
-	}
-	printk("dt_probe - my_value: %d\n", my_value);
+	var2 = (((((raw_temp >> 4) - (dig_T1)) * ((raw_temp >> 4) - (dig_T1))) >> 12) * (dig_T3)) >> 14;
+	return ((var1 + var2) *5 +128) >> 8;
+}
 
+/**
+ * @brief Get data out of buffer
+ */
+static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+	char out_string[20];
+	int temperature;
+
+	/* Get amount of bytes to copy */
+	to_copy = min(sizeof(out_string), count);
+
+	/* Get temperature */
+	temperature = read_temperature();
+	snprintf(out_string, sizeof(out_string), "%d.%d\n", temperature/100, temperature%100);
+
+	/* Copy Data to user */
+	not_copied = copy_to_user(user_buffer, out_string, to_copy);
+
+	/* Calculate delta */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+/**
+ * @brief This function is called, when the device file is opened
+ */
+static int driver_open(struct inode *deviceFile, struct file *instance) {
+	printk("MyDeviceDriver -  Open was called\n");
 	return 0;
 }
 
 /**
- * @brief This function is called on unloading the driver 
+ * @brief This function is called, when the device file is close
  */
-static int dt_remove(struct platform_device *pdev) {
-	printk("dt_probe - Now I am in the remove function\n");
+static int driver_close(struct inode *deviceFile, struct file *instance) {
+	printk("MyDeviceDriver -  Close was called\n");
 	return 0;
 }
 
+/* Map the file operations */
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = driver_open,
+	.release = driver_close,
+	.read = driver_read,
+};
+
+
 /**
- * @brief This function is called, when the module is loaded into the kernel
+ * @brief function, which is called after loading module to kernel, do initialization there
  */
-static int __init my_init(void) {
-	printk("dt_probe - Loading the driver...\n");
-	if(platform_driver_register(&my_driver)) {
-		printk("dt_probe - Error! Could not load driver\n");
-		return -1;
+static int __init ModuleInit(void) {
+	int ret = -1;
+	u8 id;
+	printk("MyDeviceDriver - Hello Kernel\n");
+
+	/* Allocate Device Nr */
+	if ( alloc_chrdev_region(&myDeviceNr, 0, 1, DRIVER_NAME) < 0) {
+		printk("Device Nr. could not be allocated!\n");
 	}
-	return 0;
+	printk("MyDeviceDriver - Device Nr %d was registered\n", myDeviceNr);
+
+	/* Create Device Class */
+	if ((myClass = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+		printk("Device Class can not be created!\n");
+		goto ClassError;
+	}
+
+	/* Create Device file */
+	if (device_create(myClass, NULL, myDeviceNr, NULL, DRIVER_NAME) == NULL) {
+		printk("Can not create device file!\n");
+		goto FileError;
+	}
+
+	/* Initialize Device file */
+	cdev_init(&myDevice, &fops);
+
+	/* register device to kernel */
+	if (cdev_add(&myDevice, myDeviceNr, 1) == -1) {
+		printk("Registering of device to kernel failed!\n");
+		goto KernelError;
+	}
+
+	bmp_i2c_adapter = i2c_get_adapter(I2C_BUS_AVAILABLE);
+
+	if(bmp_i2c_adapter != NULL) {
+		bmp280_i2c_client = i2c_new_device(bmp_i2c_adapter, &bmp_i2c_board_info);
+		if(bmp280_i2c_client != NULL) {
+			if(i2c_add_driver(&bmp_driver) != -1) {
+				ret = 0;
+			}
+			else
+				printk("Can't add driver...\n");
+                return 0;
+		}
+		i2c_put_adapter(bmp_i2c_adapter);
+	}
+	printk("BMP280 Driver added!\n");
+
+	/* Read Chip ID */
+	id = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xD0);
+	printk("ID: 0x%x\n", id);
+
+
+	return ret;
+
+KernelError:
+	device_destroy(myClass, myDeviceNr);
+FileError:
+	class_destroy(myClass);
+ClassError:
+	unregister_chrdev(myDeviceNr, DRIVER_NAME);
+	return (-1);
 }
 
 /**
- * @brief This function is called, when the module is removed from the kernel
+ * @brief function, which is called when removing module from kernel
+ * free alocated resources
  */
-static void __exit my_exit(void) {
-	printk("dt_probe - Unload driver");
-	platform_driver_unregister(&my_driver);
+static void __exit ModuleExit(void) {
+	printk("MyDeviceDriver - Goodbye, Kernel!\n");
+	i2c_unregister_device(bmp280_i2c_client);
+	i2c_del_driver(&bmp_driver);
+	cdev_del(&myDevice);
+    device_destroy(myClass, myDeviceNr);
+    class_destroy(myClass);
+    unregister_chrdev_region(myDeviceNr, 1);
 }
 
-module_init(my_init);
-module_exit(my_exit);
-
-
+module_init(ModuleInit);
+module_exit(ModuleExit);
